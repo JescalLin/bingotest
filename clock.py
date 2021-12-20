17e2c660b669882e98a12ad3b27081e95d794440
Jescal Lin <Jescal0001@gmail.com>
2021-12-20 09:13:31 +0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -4,9 +4,9 @@ import time
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=10)
+@sched.scheduled_job('interval', minutes=60)
 def timed_job_awake_your_app():
-    print('awake app every 10 minutes.')
+    print('awake app every 60 minutes.')
     url = 'https://bingoflask.herokuapp.com/'
     r = requests.get(url)
     print("--> r.content")
