a8e84b3d6fc310b7c57447c1487392da278693f1
Jescal Lin <Jescal0001@gmail.com>
2022-01-14 11:32:17 +0800

    add

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import imp
 from flask import Flask, render_template, request, redirect, url_for, make_response,jsonify
 from werkzeug.utils import secure_filename
 from datetime import timedelta
@@ -13,6 +14,7 @@ from itertools import combinations
 import cv2
 import math
 import pandas as pd
+import csv
 
 target = 0
 
@@ -48,6 +50,82 @@ def stock():
 
         return render_template('stock_ok.html',target=target,log=log,best=best)
     return render_template('stock.html')
+
+
+
+@app.route('/lottery', methods=['POST', 'GET'])  
+def lottery():
+    if request.method == 'POST':
+        target = int(request.form.get('target'))
+        time_date = list()
+        num_data = list()
+        with open('./data/大樂透_2021.csv', newline='' ,encoding="utf-8") as csvfile:
+            # 讀取 CSV 檔內容，將每一列轉成一個 dictionary
+            rows = csv.DictReader(csvfile)
+            # 以迴圈輸出指定欄位
+            for row in rows:
+                time_date.append(row['期別'])
+                num_data.append([int(row['獎號1']),int(row['獎號2']),int(row['獎號3']),int(row['獎號4']),int(row['獎號5']),int(row['獎號6']),int(row['特別號'])])
+
+        num_data = num_data[::-1]
+        num_data = num_data[0:target]
+
+
+        #二球熱門組合
+        array2_all = []
+        array2_temp = []
+        for i in range(target):
+            array2_temp = list(combinations(num_data[i], 2))
+            for j in range(len(array2_temp)):
+                array2_all.append(array2_temp[j])
+        d2 = Counter(array2_all)
+        sorted_x2 = sorted(d2.items(), key=lambda x2: x2[1], reverse=True)
+        #三球熱門組合
+        array3_all = []
+        array3_temp = []
+        for i in range(target):
+            array3_temp = list(combinations(num_data[i], 3))
+            for j in range(len(array3_temp)):
+                array3_all.append(array3_temp[j])
+        d3 = Counter(array3_all)
+        sorted_x3 = sorted(d3.items(), key=lambda x3: x3[1], reverse=True)
+        print(str(sorted_x2))
+        print(str(sorted_x3))
+
+
+        #連續未開
+        temp_array = []
+        for k in range(target):
+            b_target = [0]*49
+            for i in range(49):
+                ball_num = i+1
+                if ball_num in num_data[k]:
+                    b_target[i] = 1
+                else:
+                    b_target[i] = 0
+            temp_array.append(b_target)
+        b_count = [0]*49
+        for k in range(49):
+            for i in range(len(temp_array)):
+                if temp_array[i][k]==0:
+                    b_count[k]=b_count[k]+1
+                else:
+                    break
+        noopen_num = ""
+        for i in range(49):
+            noopen_num = noopen_num + str(i+1)+":"+str(b_count[i])+"</br>"
+        tmp = max(b_count)
+        index = b_count.index(tmp)
+        index = str(index+1)
+        noopen_num = noopen_num+"最多期未開號碼"+str(index)+"</br>"
+
+
+
+
+        return render_template('lottery_ok.html',target=target,connum2=str(sorted_x2),connum3=str(sorted_x3),noopen_num=noopen_num)
+    return render_template('lottery.html')
+
+
 
 @app.route('/bingo', methods=['POST', 'GET'])  
 def bingo():
--- test.py
+++ test.py
@@ -14,29 +14,94 @@ from itertools import combinations
 import cv2
 import math
 import pandas as pd
+import csv
 
-lingu_fee_o = 0
-lingu_fee = 0
 
-targer = 17.9
-fee = 0.001425
-log = ""
-temp_min = 100000
-temp_index = 0
-for i in range(1000):
-    input_v = i+1
-    input_count = math.ceil(1000/input_v)
-    lingu_fee_o = input_v*fee*targer
-    lingu_fee = math.floor(lingu_fee_o)
-    if(lingu_fee<2):
-        lingu_fee = 1
-    if lingu_fee*input_count > temp_min:
-        pass
-    else:
-        temp_min = lingu_fee*input_count
-        temp_index = input_v
-    log = log + " "+str(input_v)+" "+str(input_count)+" "+str(lingu_fee)+" "+str(lingu_fee*input_count)+"\n"
 
-print("最佳零股:"+str(temp_index)+" 手續費:"+str(temp_min))
+# url="https://lotto.auzonet.com/bingobingo/list_20220113.html"
+# all_num_array = list()
+# time_date = list()
+# num_data = list()
+# res  = requests.get(url)
+# soup = BeautifulSoup(res.text,'html.parser')
+# table = soup.find('table', {'bordercolor': '#C0C0C0'})
+# trs = table.find_all('tr', {'class': 'bingo_row'})
+# tds = table.find_all('td', {'class': 'BPeriod'})
+# for td in tds:
+#     time_date.append(td.text[-5:])
+# for tr in trs:
+#     temp_row = list()
+#     temp_show_row = list()
+#     for div in tr.find_all('div'):
+#         temp_row.append(int(div.text))
+#     num_data.append(temp_row)
 
-print(log)
+# print(num_data)
+
+
+
+
+
+
+time_date = list()
+num_data = list()
+target = 10
+with open('./data/大樂透_2021.csv', newline='' ,encoding="utf-8") as csvfile:
+
+  # 讀取 CSV 檔內容，將每一列轉成一個 dictionary
+  rows = csv.DictReader(csvfile)
+  # 以迴圈輸出指定欄位
+  for row in rows:
+    time_date.append(row['期別'])
+    num_data.append([int(row['獎號1']),int(row['獎號2']),int(row['獎號3']),int(row['獎號4']),int(row['獎號5']),int(row['獎號6']),int(row['特別號'])])
+
+
+#二球熱門組合
+array2_all = []
+array2_temp = []
+for i in range(target):
+    array2_temp = list(combinations(num_data[i], 2))
+    for j in range(len(array2_temp)):
+        array2_all.append(array2_temp[j])
+d2 = Counter(array2_all)
+sorted_x2 = sorted(d2.items(), key=lambda x2: x2[1], reverse=True)
+#三球熱門組合
+array3_all = []
+array3_temp = []
+for i in range(target):
+    array3_temp = list(combinations(num_data[i], 3))
+    for j in range(len(array3_temp)):
+        array3_all.append(array3_temp[j])
+d3 = Counter(array3_all)
+sorted_x3 = sorted(d3.items(), key=lambda x3: x3[1], reverse=True)
+print(str(sorted_x2))
+print(str(sorted_x3))
+
+
+#連續未開
+temp_array = []
+for k in range(target):
+    b_target = [0]*49
+    for i in range(49):
+        ball_num = i+1
+        if ball_num in num_data[k]:
+            b_target[i] = 1
+        else:
+            b_target[i] = 0
+    temp_array.append(b_target)
+b_count = [0]*49
+for k in range(49):
+    for i in range(len(temp_array)):
+        if temp_array[i][k]==0:
+            b_count[k]=b_count[k]+1
+        else:
+            break
+noopen_num = ""
+for i in range(49):
+    noopen_num = noopen_num + str(i+1)+":"+str(b_count[i])+"</br>"
+tmp = max(b_count)
+index = b_count.index(tmp)
+index = str(index+1)
+noopen_num = noopen_num+"最多期未開號碼"+str(index)+"</br>"
+
+print(noopen_num)
